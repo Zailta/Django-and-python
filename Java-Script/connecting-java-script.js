2104f8ab927159217d305858a7453dbe044cba68
Zailta <mannanbhat60@gmail.com>
2021-02-22 16:25:36 +0530

    commit #24

--- Java-Script/connecting-java-script.js
+++ Java-Script/connecting-java-script.js
@@ -1,24 +1,35 @@
-alert("welcome to your bank")
-var deposit =  prompt("how much do you want to add in your account")
-alert("you have deposited"+deposit)
-console.log("you are a cool person")
-alert("enter the weight in lbs")
-var lb = prompt("please eneter a number for conversion")
-var kg = lb*0.453592
-alert("The conversion amount in KGs is "+kg)
-console.log("conversion complete")
-var x = 0
-word ="hello"
-while(x< word.length){
-  console.log(wor[i])
-  x = x+1
-
+// alert("welcome to your bank")
+// var deposit =  prompt("how much do you want to add in your account")
+// alert("you have deposited"+deposit)
+// console.log("you are a cool person")
+// alert("enter the weight in lbs")
+// var lb = prompt("please eneter a number for conversion")
+// var kg = lb*0.453592
+// alert("The conversion amount in KGs is "+kg)
+// console.log("conversion complete")
+// var x = 0
+// word ="hello"
+// while(x< word.length){
+//   console.log(wor[i])
+//   x = x+1
+//
+// }
+// for(var i =0; i<5; i++){
+//   console.log("hello");
+// }
+// for(var i =1; i<=25; i++){
+//   if(i%2 != 0){
+//     console.log(i);
+//   }
+// }
+var first = prompt("Enter Your First Name")
+var last = prompt("Enter Your Last Name")
+var age = prompt("Enter Your Age")
+var height = prompt("Enter Your height")
+var pet_name = prompt("Enter Your Pet-Name")
+if((first==last) && (age>20 && age<30) && (height== 170) && (pet_name[pet_name.length-1]=== "y")){
+  alert("spy detected")
 }
-for(var i =0; i<5; i++){
-  console.log("hello");
-}
-for(var i =1; i<=25; i++){
-  if(i%2 != 0){
-    console.log(i);
-  }
+else {
+  alert("you got the wrong guy")
 }
