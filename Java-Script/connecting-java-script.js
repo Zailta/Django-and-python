cc2cc3cf951f64f098c6058aa9a3c34ec5fca374
Zailta <mannanbhat60@gmail.com>
2021-02-22 16:03:18 +0530

    commit#23

--- Java-Script/connecting-java-script.js
+++ Java-Script/connecting-java-script.js
@@ -2,3 +2,23 @@ alert("welcome to your bank")
 var deposit =  prompt("how much do you want to add in your account")
 alert("you have deposited"+deposit)
 console.log("you are a cool person")
+alert("enter the weight in lbs")
+var lb = prompt("please eneter a number for conversion")
+var kg = lb*0.453592
+alert("The conversion amount in KGs is "+kg)
+console.log("conversion complete")
+var x = 0
+word ="hello"
+while(x< word.length){
+  console.log(wor[i])
+  x = x+1
+
+}
+for(var i =0; i<5; i++){
+  console.log("hello");
+}
+for(var i =1; i<=25; i++){
+  if(i%2 != 0){
+    console.log(i);
+  }
+}
