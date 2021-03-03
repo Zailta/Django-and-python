c790d66b1f08526538f9ea33242746b02db6e1ac
Zailta <mannanbhat60@gmail.com>
2021-03-03 13:42:55 +0530

    commit #29

--- Java-Script/DOM-assesment.js
+++ Java-Script/DOM-assesment.js
@@ -1,1 +1,24 @@
 console.log("hello");
+var button = document.querySelector("#button");
+
+var thead = document.querySelectorAll('td');
+button.addEventListener("click", function(){
+  for(var i = 0; i<thead.length; i++){
+    thead[i].textContent= '';
+  }
+});
+
+for(var i = 0; i< thead.length;i++){
+  thead[i].addEventListener("click", function(){
+    if(this.textContent === ''){
+      this.textContent = 'X'
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O'
+
+    }
+    else  {
+      this.textContent = ''
+
+    }
+  })
+}
